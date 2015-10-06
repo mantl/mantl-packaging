e8df4ef003042eeaa410f8e765e29e36f46f3449
Brian Hicks <brian@brianthicks.com>
2015-10-06 14:55:36 -0500

    build.sh: add

---
+++ build.sh
@@ -0,0 +1,18 @@
+#/usr/bin/env bash
+# make sure the VM is running
+if [ "$(vagrant status | grep 'running')" == "" ]; then
+    vagrant up
+fi
+
+# setup
+SSH_CONFIG=$(mktemp /tmp/ssh-config.XXXXX)
+vagrant ssh-config > $SSH_CONFIG
+
+# execute
+ssh -F $SSH_CONFIG default "/home/vagrant/go/bin/hammer build --search=/vagrant --output=/vagrant/out $*"
+STATUS=$?
+
+# cleanup
+rm $SSH_CONFIG
+
+exit $STATUS
