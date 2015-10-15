6afa28aacd75bbd6a68a05331d25fa077e1be242
Brian Hicks <brian@brianthicks.com>
2015-10-15 17:02:50 -0500

    scripts(bintray): automatically publish

--- scripts/bintray.sh
+++ scripts/bintray.sh
@@ -15,6 +15,10 @@ for PKG in $(ls $OUTPUT); do
     RESP=$(curl -T $OUTPUT/$PKG -u$BINTRAY_USER:$BINTRAY_API_KEY $URL)
     if [[ "$RESP" == '{"message":"success"}' ]]; then
         echo "$PKG:	succesfully uploaded"
+
+        echo "$PKG:\tpublishing"
+        PUBLISH_URL=$(echo $PKG | sed -E "s|^([a-zA-Z\-]+)-([0-9\.\-]+)\..*$|https://api.bintray.com/content/$BINTRAY_PROJECT/\1/\2/publish|")
+        curl -u$BINTRAY_USER:$BINTRAY_API_KEY -X POST $PUBLISH_URL
     else
         echo "$PKG:	error uploading"
         echo $RESP
