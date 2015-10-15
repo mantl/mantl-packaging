627b37b4075570d4a3d9bc7ec9c554e2f33f258b
Brian Hicks <brian@brianthicks.com>
2015-10-15 16:54:47 -0500

    scripts(bintray): update with new response

--- scripts/bintray.sh
+++ scripts/bintray.sh
@@ -13,7 +13,7 @@ for PKG in $(ls $OUTPUT); do
     echo "$PKG:\tuploading"
     URL=$(echo $PKG | sed -E "s|^(([a-zA-Z\-]+)-([0-9\.\-]+)\..*)$|https://api.bintray.com/content/$BINTRAY_PROJECT/\2/\3/\1|")
     RESP=$(curl -T $OUTPUT/$PKG -u$BINTRAY_USER:$BINTRAY_API_KEY $URL)
-    if [[ "$RESP" == "{}" ]]; then
+    if [[ "$RESP" == '{"message":"success"}' ]]; then
         echo "$PKG:	succesfully uploaded"
     else
         echo "$PKG:	error uploading"
