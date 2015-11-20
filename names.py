63bed27227f002f8b482974cc8e6d009b771c8e9
Brian Hicks <brian@brianthicks.com>
2015-11-20 17:15:47 -0600

    travis: add last build caching

---
+++ names.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+import sys
+from subprocess import Popen, PIPE
+
+prefixes = {
+    'consul/consul-cli': 'consul-cli',
+    'consul/consul': 'consul',
+    'consul/consul-template': 'consul-template',
+    'nomad/nomad': 'nomad',
+    'packages/docker-cleanup': 'docker-cleanup',
+    'packages/generate-certificate': 'generate-certificate',
+    'packages/traefik': 'traefik',
+}
+
+def diffed_files(a, b):
+    """get the diffed files between two commits"""
+    git = Popen(["git", "diff", "--name-only", a, b], stdout=PIPE, stderr=PIPE)
+    out, err = git.communicate()
+
+    return out.split()
+
+def main(args):
+    names = set()
+    for fname in diffed_files(*args):
+        for prefix, package in prefixes.items():
+            if fname.startswith(prefix):
+                names.add(package)
+
+    print ' '.join(names)
+
+    return 0
+
+if __name__ == "__main__":
+    sys.exit(main(sys.argv[1:]))
