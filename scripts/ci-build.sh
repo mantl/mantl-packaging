dda391a6ea110a8f25b41e027bea28c013ff6339
Brian Hicks <brian@brianthicks.com>
2016-02-09 08:31:29 -0600

    travis: simplify built paths

--- scripts/ci-build.sh
+++ scripts/ci-build.sh
@@ -11,7 +11,7 @@ else
     echo "no 'ci: all' found in any range commit message, evaluating whether or not to build based on changed files"
 
     CHANGED_FILES=$(git diff --name-only $TRAVIS_COMMIT_RANGE)
-    PACKAGE_ROOT=$(grep "|$PACKAGE|" ./scripts/paths | sed "s|$(pwd)/||g" | cut -f 2 -d ' ')
+    PACKAGE_ROOT=$(grep "|$PACKAGE|" ./scripts/paths | cut -f 2 -d ' ')
 
     if ! $(echo $CHANGED_FILES | grep -q $PACKAGE_ROOT); then
         echo "exiting now because there are no changed files in ${TRAVIS_COMMIT_RANGE} for ${PACKAGE}"
